3f67aebdc42334966fd4796e07d7945d8a32a832
Bankole Ojo <ojobankole@gmail.com>
2023-03-26 19:50:10 +0100

    updated the flstore launcher to absolute path

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/getlantern/systray"
 )
@@ -16,6 +17,11 @@ func main() {
 }
 
 func onReady() {
+	go func() {
+		absFlStorePath, _ := filepath.Abs("flstore.exe")
+		exec.Command(absFlStorePath).Run()
+	}()
+
 	systray.SetIcon(logoBytes)
 	systray.SetTitle("Flaarum")
 	systray.SetTooltip("Flaarum: a more comfortable database")
@@ -25,13 +31,22 @@ func onReady() {
 		<-flaarumTuts.ClickedCh
 		exec.Command("cmd", "/C", "start", "https://sae.ng/flaarumtuts").Run()
 	}()
+	systray.AddSeparator()
 
+	openFlaarumFolder := systray.AddMenuItem("Open Flaarum folder", "Open Flaarum folder")
+	go func() {
+		<-openFlaarumFolder.ClickedCh
+		hd, _ := os.UserHomeDir()
+		flaarumFolder := filepath.Join(hd, "Flaarum")
+		os.MkdirAll(flaarumFolder, 0777)
+		exec.Command("cmd", "/C", "start", flaarumFolder).Run()
+	}()
+
+	systray.AddSeparator()
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 	go func() {
 		<-mQuit.ClickedCh
-		fmt.Println("Requesting quit")
 		systray.Quit()
-		fmt.Println("Finished quitting")
 	}()
 
 }
